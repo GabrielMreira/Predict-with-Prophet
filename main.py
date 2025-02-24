c516a53156bdf42025da06271bfce6c0af41aefd
r0nny-dev <107933147+GabrielMreira@users.noreply.github.com>
2025-02-24 14:57:06 -0300

    feat(predict): add plot library and print plot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from prophet import Prophet
+import matplotlib.pyplot as plt
 
 np.random.seed(42)
 num_samples = 1000
@@ -19,3 +20,16 @@ future = model.make_future_dataframe(periods=24, freq='h')
 forecast = model.predict(future)
 
 forecast.to_excel("PredictData.xlsx", index=False)
+
+model.plot(forecast)
+
+plt.figure(figsize=(12, 6))
+plt.plot(df_profet['ds'], df_profet['y'], label="Dados Reais", color="blue")
+plt.plot(forecast['ds'], forecast['yhat'], label="Previsão", color="red", linestyle="dashed")
+plt.xlabel("Data")
+plt.ylabel("Temperatura (°C)")
+plt.title("Previsão de Temperatura com Prophet")
+plt.legend()
+plt.grid()
+
+plt.show()
